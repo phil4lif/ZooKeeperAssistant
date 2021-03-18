17d4235c7e5cb3ec3d7965608a6a2d6936d91285
phil cowan <philipjcowan1@gmail.com>
2021-03-18 11:03:45 -0700

    adds filter by query function to return filtered results

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const { animals } = require('./data/animals');
+
 
 const app = express();
 
 const PORT = 3001;
 
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet)
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+})
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 });
