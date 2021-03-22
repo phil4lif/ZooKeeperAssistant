a697608caf75028ecf5f9db026cd5f256e38203a
phil cowan <philipjcowan1@gmail.com>
2021-03-22 11:25:51 -0700

    displaying all animals on the front end

--- server.js
+++ server.js
@@ -102,6 +102,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 });
