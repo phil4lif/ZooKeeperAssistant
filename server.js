4266fd460c310d4196bfc57d20ddcfdeefafac0a
phil cowan <philipjcowan1@gmail.com>
2021-03-22 10:42:51 -0700

    adds all the front end files

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3001;
 
@@ -96,6 +97,10 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
