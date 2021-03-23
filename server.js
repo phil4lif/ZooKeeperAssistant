9a9de5f71633162b62b6271de25979fdf9c8d740
phil cowan <philipjcowan1@gmail.com>
2021-03-23 09:05:04 -0700

    moves api routes into their own folder and file

--- server.js
+++ server.js
@@ -11,33 +11,7 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-})
 
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.sendStatus(404);
-    }
-});
-
-app.post('/api/animals', (req, res) => {
-    req.body.id = animals.length.toString();
-
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal data is not properly formatted')
-    } else {
-        const animal = createNewAnimal(req.body, animals);
-        res.json(animal);
-    }
-})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
